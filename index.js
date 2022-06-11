1fa9eee52711efee0c690a03bdae2637fb921047
davidnene <davienesh4@gmail.com>
2022-06-11 22:52:12 +0300

    commit tree 3061c416dc809e79e45f072dd4dc2031a1060cff

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+function print(value) {
+  return console.info(value);
+};
+
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
+const [bessie,, dolly, babe, little] = farmAnimals.split(' ')
+
+const [blackAndWhite,, black,pink,] = farmAnimals.split(' ')
+print(blackAndWhite)
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+const[r, o, y, g, b, , v] = colors
+
+const[,,,,,indg,] = colors
+print(indg);
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +26,10 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName, color, song, job, partner} = muppet
+print(muppetName)
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +45,11 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+const {nestedJob, nestedPartner} = nestedMuppet
+
+print(song2)
 
 // Strings
 
